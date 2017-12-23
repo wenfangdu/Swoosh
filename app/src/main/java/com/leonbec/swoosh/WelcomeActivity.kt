c8610114631fdaf58dc7e17ba42673ab22afcecb
leonbec <dwfinz@gmail.com>
2017-12-24 11:14:01 +1300

    activity redirrection

--- app/src/main/java/com/leonbec/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/leonbec/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.leonbec.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
